4625a748668b627a1e2ca53e1ac7fcc1f1f21456
Vincent Liu <vincentliu@skyscanner.net>
2021-12-28 13:49:37 +0800

    remove useless element

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-version">
+          v 1.0.0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Dockerizing CRA.
+        </p>
       </header>
     </div>
   );
